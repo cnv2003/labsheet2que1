d2343b637f41a915818bce8513fe380c2a45f735
cnv2003 <chalanika03@gmail.com>
2024-05-06 19:43:24 +0530

    Add project files.

---
+++ labsheet2que1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet2que1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            public class book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Title:{Title}");
+                Console.WriteLine($"Author:{Author}");
+            }
+        }
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+                book book = new book();
+                book.Title = "Harry Potter";
+                book.Author = "J.K.Rowling";
+                Console.WriteLine("\nBook Information:");
+                book.DisplayInfo();
+            }
+        }
+    }
+}
